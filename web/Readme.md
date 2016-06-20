b72d758cc2194083bde8566c926424b223a9b486
muyun <wenlzhao@gmail.com>
2016-06-20 22:57:14 +0800

    update

--- web/Readme.md
+++ web/Readme.md
@@ -1,4 +1,10 @@
 #### Flask web
 
+- Use the latest version -> flask --version
+-  running
+   export FLASK_APP=flaskr
+   python -m flask run
+
+- configure   
 
 #### reference
--- web/simptext/simptext.py
+++ web/simptext/simptext.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 import os
 import sqlite3
+#from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort,\
     render_template, flash
 
 # the application
 app = Flask(__name__)
-app.config.from_object(__name)
+app.config.from_object(__name__)
 
 # load default config
 app.config.update(dict(
     # the Database Path
     DATABASE=os.path.join(app.root_path,'simptext.db'),
+    DEBUG=True,
     SECRET_KEY='development key',
     USERNAME='admin',
     PASSWORD='default'
@@ -29,5 +31,77 @@ def connect_db():
     return rv
 
 
+def init_db():
+    """initializes the database"""
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """create the database tables"""
+    init_db()
+    print 'Initialized the database'
+
 def get_db():
-    """ opens a new database connection
+    """ opens a new database connection """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+"""
+functions marked with teardown_appcontext() are called
+"""
+@app.teardown_appcontext
+def close_db(error):
+    """close the database again at the end of the request"""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+#the view functions - the view shows all the netries stored in the database
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+# this view let the user add new entries if they are logged in
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries(title, text) values (?, ?)',
+               [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+
+#login and logout
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+if __name__ == '__main__':
+    app.run()
