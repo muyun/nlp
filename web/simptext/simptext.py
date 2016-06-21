6d61ebc4055acb19a9c0b69a330a4b69420ca622
muyun <wenlzhao@gmail.com>
2016-06-21 16:57:12 +0800

    update

--- web/simptext/simptext.py
+++ web/simptext/simptext.py
@@ -16,7 +16,7 @@ app.config.update(dict(
     DEBUG=True,
     SECRET_KEY='development key',
     USERNAME='admin',
-    PASSWORD='default'
+    PASSWORD='admin'
 ))
 
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
@@ -64,7 +64,7 @@ def close_db(error):
 @app.route('/')
 def show_entries():
     db = get_db()
-    cur = db.execute('select title, text from entries order by id desc')
+    cur = db.execute('select title, input from entries order by id desc')
     entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries)
 
@@ -74,8 +74,8 @@ def add_entry():
     if not session.get('logged_in'):
         abort(401)
     db = get_db()
-    db.execute('insert into entries(title, text) values (?, ?)',
-               [request.form['title'], request.form['text']])
+    db.execute('insert into entries(title, input) values (?, ?)',
+               [request.form['title'], request.form['input']])
     db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
