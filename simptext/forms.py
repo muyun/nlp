7c9e2ed35d82fd70f74eb77ab7fd91c55af4bcde
muyun <wenlzhao@gmail.com>
2016-07-14 22:29:09 +0800

    add the file

---
+++ simptext/forms.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+  the login form
+  TODO: the registration form
+
+@author wenlong
+"""
+
+from wtforms import Form, StringField, SubmitField, PasswordField, validators
+
+
+class LoginForm(Form):
+    username=StringField('username', [validators.Length(min=4, max=25)])
+    #password= PasswordField('password',[validators.DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class EntryForm(Form):
+    text=StringField('username', [validators.Length(min=4, max=25)])
+    #password= PasswordField('password',[validators.DataRequired()])
+    submit = SubmitField('Submit')
